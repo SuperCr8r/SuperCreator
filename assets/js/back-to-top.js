63f27ba479814a1a6fe36323aca53ca14bf45b95
SuperCr8r <supercreator0051@gmail.com>
2026-03-09 17:01:10 +0530

    Create back-to-top.js

---
+++ assets/js/back-to-top.js
@@ -0,0 +1,16 @@
+let mybutton = document.getElementById("backToTopBtn");
+
+window.onscroll = function() {
+if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+};
+
+mybutton.onclick = function() {
+window.scrollTo({
+top: 0,
+behavior: "smooth"
+});
+};
